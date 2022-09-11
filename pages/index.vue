9f0f08500a4c4de3df31e3b7d8922f8e634c0a33
becardine <brenda_alves@live.com>
2022-09-11 15:39:03 -0300

    create homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,37 @@
 <template>
   <div>
     <Nav />
+    <HomeCarousel :sale_items="sale_items" />
+    <br />
+    <br />
+    <v-container>
+      <h1 class="text-md-h4 text-h6">Check these out</h1>
+      <br />
+      <ProductSlider :products="products" />
+      <br><br><br>
+      <Newsletter />
+    </v-container>
     <TheFooter />
+    <ScrollTop />
   </div>
 </template>
 
 <script>
 export default {
   async created() {
-    this.products = await this.$content('products').fetch();
-    this.sale_items = await this.$content('products').where({onSale: true}).fetch();
+    this.products = await this.$content("products").fetch();
+    this.sale_items = await this.$content("products")
+      .where({ onSale: true })
+      .fetch();
   },
   data() {
     return {
       products: null,
       sale_items: null,
-    }
+    };
   },
-}
+};
 </script>
 
 <style>
-
 </style>
